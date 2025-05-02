ee990dc4cf74322b384efef6df4872b5b5c54ca6
Tiny Brick Boy <102919798+TinyBrickBoy@users.noreply.github.com>
2025-05-02 16:25:26 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,25 @@
-#!/bin/bash
-sleep 1
+#!/bin/ash
+# Sicherstellen, dass der Skript bei Fehlern stoppt
+set -e
 
 cd /home/container
 
+# Überprüfen, ob vendor/autoload.php existiert und Composer-Abhängigkeiten installieren
+if [ -f "composer.json" ]; then
+  if [ ! -f "vendor/autoload.php" ]; then
+    echo "vendor/autoload.php nicht gefunden. Führe composer install aus..."
+    composer install --no-interaction --no-progress
+    
+    # Falls immer noch nicht vorhanden, versuche es mit einem erneuten Dump
+    if [ ! -f "vendor/autoload.php" ]; then
+      echo "Versuche composer dump-autoload..."
+      composer dump-autoload --optimize
+    fi
+  fi
+fi
+
 # Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+MODIFIED_STARTUP=$(eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
